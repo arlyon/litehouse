40ea1b00836c269828d11db7dc8717f74a7279be
Alexander Lyon <arlyon@me.com>
2024-05-09 13:03:13 +0100

    add download to code blocks

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -1,9 +1,17 @@
-import type { MDXComponents } from 'mdx/types';
-import defaultComponents from 'fumadocs-ui/mdx';
+import type { MDXComponents } from "mdx/types";
+import defaultComponents from "fumadocs-ui/mdx";
+import { Pre, CodeBlock } from "@/components/code-block";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    pre: ({ ref: _ref, icon, title, ...props }: any) =>
+       (
+         <CodeBlock icon={icon} title={title} allowDL={title?.includes(".")}>
+          <Pre {...props} />
+        </CodeBlock>
+      )
+
   };
 }
