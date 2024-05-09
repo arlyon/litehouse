198f1f431fc0b54a68dfb1d16e44812593346327
Alexander Lyon <arlyon@me.com>
2024-05-09 13:08:21 +0100

    add biome to project

--- site/components/scroll-area.tsx
+++ site/components/scroll-area.tsx
@@ -3,54 +3,54 @@ import * as React from "react";
 import { twMerge as cn } from "tailwind-merge";
 
 const ScrollArea = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+	React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
 >(({ className, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn("overflow-hidden", className)}
-    {...props}
-  >
-    {children}
-    <ScrollAreaPrimitive.Corner />
-    <ScrollBar orientation="vertical" />
-  </ScrollAreaPrimitive.Root>
+	<ScrollAreaPrimitive.Root
+		ref={ref}
+		className={cn("overflow-hidden", className)}
+		{...props}
+	>
+		{children}
+		<ScrollAreaPrimitive.Corner />
+		<ScrollBar orientation="vertical" />
+	</ScrollAreaPrimitive.Root>
 ));
 
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
 
 const ScrollViewport = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Viewport>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Viewport>
+	React.ElementRef<typeof ScrollAreaPrimitive.Viewport>,
+	React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Viewport>
 >(({ className, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Viewport
-    ref={ref}
-    className={cn("size-full rounded-[inherit]", className)}
-    {...props}
-  >
-    {children}
-  </ScrollAreaPrimitive.Viewport>
+	<ScrollAreaPrimitive.Viewport
+		ref={ref}
+		className={cn("size-full rounded-[inherit]", className)}
+		{...props}
+	>
+		{children}
+	</ScrollAreaPrimitive.Viewport>
 ));
 
 ScrollViewport.displayName = ScrollAreaPrimitive.Viewport.displayName;
 
 const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Scrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Scrollbar>
+	React.ElementRef<typeof ScrollAreaPrimitive.Scrollbar>,
+	React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Scrollbar>
 >(({ className, orientation = "vertical", ...props }, ref) => (
-  <ScrollAreaPrimitive.Scrollbar
-    ref={ref}
-    orientation={orientation}
-    className={cn(
-      "flex select-none data-[state=hidden]:animate-fade-out",
-      orientation === "vertical" && "h-full w-1.5",
-      orientation === "horizontal" && "h-1.5 flex-col",
-      className,
-    )}
-    {...props}
-  >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
-  </ScrollAreaPrimitive.Scrollbar>
+	<ScrollAreaPrimitive.Scrollbar
+		ref={ref}
+		orientation={orientation}
+		className={cn(
+			"flex select-none data-[state=hidden]:animate-fade-out",
+			orientation === "vertical" && "h-full w-1.5",
+			orientation === "horizontal" && "h-1.5 flex-col",
+			className,
+		)}
+		{...props}
+	>
+		<ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+	</ScrollAreaPrimitive.Scrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.Scrollbar.displayName;
 
--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -1,17 +1,15 @@
-import type { MDXComponents } from "mdx/types";
+import { CodeBlock, Pre } from "@/components/code-block";
 import defaultComponents from "fumadocs-ui/mdx";
-import { Pre, CodeBlock } from "@/components/code-block";
+import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...defaultComponents,
-    ...components,
-    pre: ({ ref: _ref, icon, title, ...props }: any) =>
-       (
-         <CodeBlock icon={icon} title={title} allowDL={title?.includes(".")}>
-          <Pre {...props} />
-        </CodeBlock>
-      )
-
-  };
+	return {
+		...defaultComponents,
+		...components,
+		pre: ({ ref: _ref, icon, title, ...props }: any) => (
+			<CodeBlock icon={icon} title={title} allowDL={title?.includes(".")}>
+				<Pre {...props} />
+			</CodeBlock>
+		),
+	};
 }
