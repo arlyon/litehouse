74977795da3083d25a91bcb1e10bb4b984c18732
Alexander Lyon <arlyon@me.com>
2024-05-10 10:27:47 +0100

    add basic wit support

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,7 +1,24 @@
+import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
 import createMDX from "fumadocs-mdx/config";
 import { withAxiom } from "next-axiom";
+import { bundledLanguages } from "shiki";
 
-const withMDX = createMDX();
+const withMDX = createMDX({
+  mdxOptions: {
+    rehypeCodeOptions: {
+      langs: [
+        ...Object.keys(bundledLanguages),
+        {
+          name: "wit",
+          displayName: "Wit",
+          fileTypes: ["wit"],
+          scopeName: "source.wit",
+          patterns: [],
+        },
+      ],
+    },
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const config = {
