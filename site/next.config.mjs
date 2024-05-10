d7eb216fec299ae07166c0d477ee252306fc7515
Alexander Lyon <arlyon@me.com>
2024-05-10 11:49:29 +0100

    add proper wit support

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,20 +1,20 @@
-import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
 import createMDX from "fumadocs-mdx/config";
 import { withAxiom } from "next-axiom";
 import { bundledLanguages } from "shiki";
+import fs from "fs";
+import codeImport from 'remark-code-import';
+
+let wit = JSON.parse(fs.readFileSync('wit.tmLanguage.json', 'utf8'))
 
 const withMDX = createMDX({
   mdxOptions: {
+    remarkPlugins: [() =>codeImport({
+      allowImportingFromOutside: true
+    })],
     rehypeCodeOptions: {
       langs: [
         ...Object.keys(bundledLanguages),
-        {
-          name: "wit",
-          displayName: "Wit",
-          fileTypes: ["wit"],
-          scopeName: "source.wit",
-          patterns: [],
-        },
+        wit
       ],
     },
   },
